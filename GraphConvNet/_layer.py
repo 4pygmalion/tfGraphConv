f55a5c370d7f38d33566c7d2a3654a4a3e204bf6
4pygmalion <4pygmalion@naver.com>
2021-06-10 09:44:12 +0900

    Add calcuation the A

--- GraphConvNet/_layer.py
+++ GraphConvNet/_layer.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import networkx as nx
 
 class GraphConvolution(tf.keras.layers.Layer):
 
@@ -11,20 +12,38 @@ class GraphConvolution(tf.keras.layers.Layer):
 
 
     def _get_A_from_graph(self, edge, weight):
-        # stellargraph
-        return A
+        '''Generate the adjancy matrix from edge, weight
+
+        Parameters
+        ---------
+        edge: nested tuple or list, eg. (v1, v2)
+        weight: float
+
+        Return
+        ------
+        np.ndarray
+        '''
+        items = [(pari, w) for pair, w in zip(edge, weight)]
+        G = nx.Graph()
+        G.add_weighted_edges_from(items)
+        return nx.adjacency_matrix(G).astype(np.float32)
+
 
     def _get_degree_matrix(self, A):
         '''Element-wise inverse. D^{-1}'''
         return tf.math.reciprocal(inv_D)
 
+
     def build(self, input_shapes):
+        '''Add trainable weight W'''
         self.kernel = self.add_weight(shape=(None, output_dim), name='W', initializer=self.kernel_intializer)
         self.bias = None
         self.built = True
 
-    def call(self, X):
-        A = self._get_A_from_graph()
+
+    def call(self, X, edge, weight):
+        '''TO generate callable object. main operation'''
+        A = self._get_A_from_graph(edge, weight)
         inv_D = self._get_degree_matrix(A)
         return inv_D @ A @ X @ self.W
 
