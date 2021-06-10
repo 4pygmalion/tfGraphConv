c5736d468c0b28817d32758c8318d2ee2791a82c
4pygmalion <4pygmalion@naver.com>
2021-06-10 09:57:42 +0900

    reorganization

--- GraphConvNet/_layer.py
+++ GraphConvNet/_layer.py
@@ -47,15 +47,3 @@ class GraphConvolution(tf.keras.layers.Layer):
         inv_D = self._get_degree_matrix(A)
         return inv_D @ A @ X @ self.W
 
-
-class GCN(object):
-
-    def __init__(self, n_layer):
-        self._layers = [GraphConvolution() for _ in range(n_layer)]
-
-    def __call__(self, x):
-        h_0 = x.copy()
-        for layer in self._layers:
-            h_j = layer(h_j)
-
-        return h_j
